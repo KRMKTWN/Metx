e1f63a0e1ec074f60aa06be98df376b3a40cd3a9
KRMKTWN <37517619+KRMKTWN@users.noreply.github.com>
2025-10-06 19:31:41 -0700

    Create Footer.jsx

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="bg-black text-gray-400 py-6 mt-12 border-t border-green-800">
+      <div className="container mx-auto text-center">
+        <p>© 2025 Metx. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
